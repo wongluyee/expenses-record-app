52c33cbf76e96dac69877df01b0ab2a3a7f7fa63
Luyee Wong <wongluyee1015@hotmail.com>
2023-04-07 17:20:29 +0900

    refractor expense form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,44 @@
-import './NewExpense.css';
-import ExpenseForm from './ExpenseForm';
+import { useState } from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
-  const saveExpenseDataHandler = (enteredExpenseData) =>{
+  const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    changeAddExpenseFormState(false);
   };
 
+  const [isAddingNewExpense, changeAddExpenseFormState] = useState(false);
 
-  return(
+  const addNewExpenseHandler = () => {
+    changeAddExpenseFormState(true);
+  };
+
+  const cancelAddExpenseFormHandler = () => {
+    changeAddExpenseFormState(false);
+  };
+
+  let addNewExpenseBtn = (
+    <button onClick={addNewExpenseHandler}>Add New Expense</button>
+  );
+
+  let addNewExpenseForm;
+
+  if (isAddingNewExpense) {
+    addNewExpenseForm = (
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={cancelAddExpenseFormHandler} />
+    );
+    addNewExpenseBtn = '';
+  }
+
+  return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+      {addNewExpenseForm}
+      {addNewExpenseBtn}
     </div>
   );
 };
