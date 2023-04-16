51e575a70e17a0bcb91698c2aa21de969d8e1318
Luyee Wong <wongluyee1015@hotmail.com>
2023-04-16 17:46:54 +0900

    add validations for inputs

--- src/App.js
+++ src/App.js
@@ -36,7 +36,6 @@ const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log(expense);
     setExpenses((prevExpenses) => {
       return [expense, ...prevExpenses]
     });
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,52 +1,113 @@
-import { useState } from 'react';
+import { useState } from "react";
 import classes from "./ExpenseForm.module.css";
 
 const ExpenseForm = (props) => {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate] = useState('');
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const [isTitleValid, setIsTitleValid] = useState(true);
+  const [isAmountValid, setIsAmountValid] = useState(true);
+  const [isDateValid, setIsDateValid] = useState(true);
 
   const titleChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsTitleValid(true);
+    }
     setEnteredTitle(event.target.value);
   };
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    const amount = event.target.value;
+    if (amount >= 0) {
+      setIsAmountValid(true);
+    }
+    setEnteredAmount(amount);
   };
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+    console.log(enteredDate);
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    const expenseData = {
-      title: enteredTitle,
-      amount: +enteredAmount,
-      date: new Date(enteredDate)
-    };
-    props.onSaveExpenseData(expenseData);
-    setEnteredTitle('');
-    setEnteredAmount('');
-    setEnteredDate('');
+    if (enteredTitle.trim().length <= 0) {
+      setIsTitleValid(false);
+    }
+    if (enteredAmount <= 0) {
+      setIsAmountValid(false);
+    }
+    if (enteredDate === '') {
+      setIsDateValid(false);
+      return;
+    }
+
+    if (isTitleValid && isAmountValid && isDateValid) {
+      const expenseData = {
+        title: enteredTitle,
+        amount: +enteredAmount,
+        date: new Date(enteredDate),
+      };
+      props.onSaveExpenseData(expenseData);
+      setEnteredTitle("");
+      setEnteredAmount("");
+      setEnteredDate("");
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <div className={classes["new-expense__controls"]}>
-        <div className={classes["new-expense__control"]}>
+        <div
+          className={`${classes["new-expense__control"]} ${
+            !isTitleValid && classes.invalid
+          }`}
+        >
           <label>Title</label>
-          <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+          <div className={classes.error}>
+            {!isTitleValid ? "*Title can't be blank" : ""}
+          </div>
         </div>
-        <div className={classes["new-expense__control"]}>
+        <div
+          className={`${classes["new-expense__control"]} ${
+            !isAmountValid && classes.invalid
+          }`}
+        >
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+          <div className={classes.error}>
+            {!isAmountValid ? "*Amount can't be blank" : ""}
+          </div>
         </div>
-        <div className={classes["new-expense__control"]}>
+        <div className={`${classes["new-expense__control"]} ${
+            !isDateValid && classes.invalid
+          }`}>
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31" value={enteredDate} onChange={dateChangeHandler} />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+          <div className={classes.error}>
+            {!isDateValid ? "*Date can't be blank" : ""}
+          </div>
         </div>
       </div>
       <div className={classes["new-expense__actions"]}>
-        <button type="button" onClick={props.onCancel}>Cancel</button>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
