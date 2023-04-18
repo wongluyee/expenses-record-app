6d51e65dc560629414c53cc275a7d397116436a3
Luyee Wong <wongluyee1015@hotmail.com>
2023-04-18 10:54:13 +0900

    add delete button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,8 @@
 import classes from "./ExpenseItem.module.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 const ExpenseItem = (props) => {
   return (
@@ -11,6 +13,9 @@ const ExpenseItem = (props) => {
         <div className={classes["expense-item__price"]}>
           ¥{props.amount.toLocaleString()}
         </div>
+        <button className={classes['delete-btn']}>
+        <FontAwesomeIcon icon={faTrash} />
+        </button>
       </div>
     </Card>
   );
